6ed701275aa3f14f2eff77d3c209672eee60c54b
Chetana H R <70467404+chetanahr@users.noreply.github.com>
2025-09-20 21:49:42 +0530

    Update Solution.cs

--- Bit Manipulation/Count Binary Substrings/Solution.cs
+++ Bit Manipulation/Count Binary Substrings/Solution.cs
@@ -1,58 +1,24 @@
 public class Solution {
+    //optimising the solution because time was exceeding for larger strings
     public int CountBinarySubstrings(string s) 
     {
         int count = 0;
-        int start = 0;
-        while(start < s.Length)
+        int prev = 0;
+        int curr = 1;
+        for(int i = 1; i < s.Length ; i++)
         {
-            int end = start;
-            while(end < s.Length)
+            if(s[i] == s[i-1])
             {
-                string x = s.Substring(start, end - start+1);
-                if(HaveEqualConsecutive(x))
-                {
-                    count++;
-                }
-                end++;
-            }
-            start++;
-        }
-        return count;
-    }
-    public bool HaveEqualConsecutive(string x)
-    {
-        if(x.Length %2 != 0)
-        {
-            return false;
-        }
-        else
-        {
-            int mid = x.Length/2;
-            string s1 = x.Substring(0, mid);
-            string s2 = x.Substring(mid);
-            //if(s1 == s2)
-            //{
-                //return true;
-            //}
-            if(s1.Distinct().Count() == 1 && s2.Distinct().Count() == 1)
-            {
-                var d1 = s1.Distinct().ToArray();
-                var d2 = s2.Distinct().ToArray();
-
-                if((d1[0] == '0' && d2[0]== '1') || (d1[0] == '1' && d2[0] == '0'))
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-                
+                curr++;
             }
             else
             {
-                return false;
+                count += Math.Min(prev,curr);
+                prev = curr;
+                curr = 1;
             }
         }
+        count += Math.Min(prev,curr);
+        return count;
     }
 }
