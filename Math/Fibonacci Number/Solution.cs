ec6051455ac6789ef5ee578638c533718e6f3bfe
Chetana H R <70467404+chetanahr@users.noreply.github.com>
2026-02-19 19:59:09 +0530

    Create Solution.cs
    The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
    
    F(0) = 0, F(1) = 1
    F(n) = F(n - 1) + F(n - 2), for n > 1.
    Given n, calculate F(n).
    
     
    
    Example 1:
    
    Input: n = 2
    Output: 1
    Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
    Example 2:
    
    Input: n = 3
    Output: 2
    Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
    Example 3:
    
    Input: n = 4
    Output: 3
    Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
     
    
    Constraints:
    
    0 <= n <= 30

---
+++ Math/Fibonacci Number/Solution.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int Fib(int n) 
+    {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        int first = 0;
+        int second = 1;
+        int next = 0;
+        for(int i = 2; i <= n; i++)
+        {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        return second;
+
+        
+    }
+}
