39df2f9dfe96f312a99dfd2adc70eff4324a0e90
Chetana H R <70467404+chetanahr@users.noreply.github.com>
2026-02-24 20:58:25 +0530

    Update Solution.cs
    Optimized solution

--- String/Check If Two String Arrays are Equivalent/Solution.cs
+++ String/Check If Two String Arrays are Equivalent/Solution.cs
@@ -1,23 +1,10 @@
 public class Solution {
     public bool ArrayStringsAreEqual(string[] word1, string[] word2) 
-    {
-        string one = "";
-        foreach(var i in word1)
-        {
-            one= one+i;
-        }
-        string two = "";
-        foreach(var i in word2)
-        {
-            two = two + i;
-        }
-        if(one == two)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+    { 
+        string a = string.Join("",word1);
+        string b = string.Join("", word2);
+        if(a == b) return true;
+        else return false;
+        
     }
 }
